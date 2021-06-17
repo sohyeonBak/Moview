94c71681ccb2fc3564921d5e2ec4daa1153d6778
sohyeonBak <sohyeonbakoly@gmail.com>
2021-06-17 11:50:23 +0900

    connecting mysql

--- back/config/config.js
+++ back/config/config.js
@@ -6,21 +6,21 @@ module.exports = {
   "development": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "moview",
+    "database": "movie-review",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "test": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "moview",
+    "database": "movie-review",
     "host": "127.0.0.1",
     "dialect": "mysql"
   },
   "production": {
     "username": "root",
     "password": process.env.DB_PASSWORD,
-    "database": "moview",
+    "database": "movie-review",
     "host": "127.0.0.1",
     "dialect": "mysql"
   }
