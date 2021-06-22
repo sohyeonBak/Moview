8b48923cb369ab26a822834a2dc1faa2a000ca1d
sohyeonBak <sohyeonbakoly@gmail.com>
2021-06-22 13:23:21 +0900

    add to sassoptions in next.config.js

--- front/next.config.js
+++ front/next.config.js
@@ -1,22 +1,26 @@
 const withBundleAnalyzer = require('@next/bundle-analyzer')({
   enabled: process.env.ANALYZE === 'true',
-})
+});
 
+const path = require('path');
 
 module.exports = withBundleAnalyzer({
   compress: true,
+  sassOptions: {
+    includePaths: [path.join(__dirname, 'styles')],
+  },
   webpack(config, { webpack }){
-      const prod = process.env.NODE_ENV === 'production'
-      const plugins = [
-          ...config.plugins,
-          new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/)
-      ];
-      
-      return {
-          ...config,
-          mode: prod ? 'production' : 'development',
-          devtool : prod ? 'hidden-source-map' : 'eval',
-          plugins,
-      }
-  }
-})
+    const prod = process.env.NODE_ENV === 'production'
+    const plugins = [
+        ...config.plugins,
+        new webpack.ContextReplacementPlugin(/moment[/\\]locale$/, /^\.\/ko$/)
+    ];
+    
+    return {
+        ...config,
+        mode: prod ? 'production' : 'development',
+        devtool : prod ? 'hidden-source-map' : 'eval',
+        plugins,
+    }
+}
+});
